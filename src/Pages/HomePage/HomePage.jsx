e7d5464f6eae09a51c2d9d869f3dda442abd7356
Ahana001 <Ankita-Thakkar>
2023-07-09 20:39:36 +0530

    inital set up

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+// import { useContext } from "react";
+import "./HomePage.css";
+// import { DataContext } from "../../Context/DataContext";
+
+export function HomePage() {
+  // const { state, dispatch } = useContext(DataContext);
+
+  return <h2>Home</h2>;
+}
